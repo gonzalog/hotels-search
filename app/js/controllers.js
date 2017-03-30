e3fa75bf6fa298708d3c974471cebe8916cdfa72
Chalo <gonzalogarcia93@gmail.com>
2017-03-30 14:52:13 -0300

    Add stars filter

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -25,7 +25,14 @@ hotelsSearchControllers.controller('hotelsSearchCtrl', ['$scope', 'Hotels',
           step: 1
         }
       }
+
+      $scope.starsFilters = [...Array(5).keys()].map((n) => n+1).map((starsOption) => ({ 
+        value: starsOption,
+        selected: false,
+        applayingHotels: () => $scope.hotels.filter(({ stars }) => Number(stars) === starsOption).length
+      }))
     }
+
 
   	$scope.search = () => {
       Hotels.load($scope.currentSearch, (hotels) => $scope.hotels = hotels)
@@ -37,11 +44,20 @@ hotelsSearchControllers.controller('hotelsSearchCtrl', ['$scope', 'Hotels',
 
       const priceFilter = ({ price }) => price >= $scope.priceFilter.minValue &&
         price <= $scope.priceFilter.maxValue
+
+      const anyStarsFilter = ({ stars }) => $scope.noStarsFilter() || $scope.starsFilters.filter(
+        ({ selected }) => selected).some(({ value }) => Number(stars) === value)
       
-      return $scope.hotels.filter(nameFilter).filter(priceFilter)
+      return [nameFilter, priceFilter, anyStarsFilter].reduce(
+        (hotels, filter) => hotels.filter(filter), $scope.hotels)
     }
 
     $scope.currentSearchToLocale = (field) => $scope.currentSearch[field].toLocaleDateString()
 
     $scope.fullPrice = ({ price, discount }) => Math.trunc((price * 100) / (100 - discount ))
+
+    $scope.unselectStarsFilters = () => $scope.starsFilters.forEach(
+      (starsFilter) => starsFilter.selected = false)
+
+    $scope.noStarsFilter = () => $scope.starsFilters.every(({ selected }) => !selected)
   }]);
